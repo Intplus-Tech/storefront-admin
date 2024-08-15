3018cae2cf503f9ee0f9aad710353bfad1d7da61
drew-chidi <chidi.andrew@gmail.com>
2024-08-15 22:47:47 +0100

    setup folder structure

---
+++ app/(dashboard)/buyer-management.tsx/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const BuyerManagement = (props: Props) => {
+  return <div>BuyerManagement</div>;
+};
+
+export default BuyerManagement;
---
+++ app/(dashboard)/store-management.tsx/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const StoreManagementPage = (props: Props) => {
+  return <div>StoreManagementPage</div>;
+};
+
+export default StoreManagementPage;
