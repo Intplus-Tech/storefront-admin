602e93491a15a61fd7065f161b0012e1ccc562b2
Amiolemen David <david@piqy.co.uk>
2024-08-16 15:49:03 +0100

    added the logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,24 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Loader from "@/app/components/loader/Loader";
+import Image from "next/image";
 export default function Home() {
   const router = useRouter();
-
   useEffect(() => {
-    const redirectTimeout = setTimeout(() => {
+    const redirect = setTimeout(() => {
       router.push("/login");
     }, 2000);
-
-    return () => clearTimeout(redirectTimeout);
+    return () => clearTimeout(redirect);
   }, [router]);
   return (
-    <div className="flex h-screen  w-screen items-center justify-center">
+    <div className="flex h-screen flex-col  w-screen items-center justify-center">
+      <Image
+        alt="Storefront Logo"
+        src="/assets/images/logo.png"
+        width={207}
+        height={212}
+        className="mx-auto"
+      />
       <Loader />;
     </div>
   );
