11aa127c0489df445a05477be9426c74bb185ce2
Amiolemen David <david@piqy.co.uk>
2024-08-16 16:05:32 +0100

    added the login box

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex h-screen bg-gray-200 items-center justify-center">
+      <div className="w-[500px] h-[500px] bg-white rounded-md"></div>;
+    </div>
+  );
 };
 
 export default Login;
