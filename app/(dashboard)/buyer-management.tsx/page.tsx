b2af8ec0d4af0add63f2bfe5891a61efbabe9912
Amiolemen David <david@piqy.co.uk>
2024-09-01 12:17:25 +0100

    added headers to all the screens

--- app/(dashboard)/buyer-management.tsx/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const BuyerManagement = (props: Props) => {
-  return <div>BuyerManagement</div>;
-};
-
-export default BuyerManagement;
--- app/(dashboard)/support/page.tsx
+++ app/(dashboard)/support/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+"use client";
+import DashboardCommonHeader from "@/components/common/dashboard-common-header";
+import React from "react";
 
 type Props = {};
 
 const SupportPage = (props: Props) => {
-  return <div>SupportPage</div>;
+  return (
+    <div>
+      <DashboardCommonHeader
+        title="User/Store Support"
+        rightIcon={
+          <div
+            onClick={() => console.log("click")}
+            className=" p-3 border space-x-2 cursor-pointer border-[#979797] text-[#979797] border-gray-3  bg-white flex rounded-md items-center justify-center"
+          >
+            <p className="text-black">Export </p>
+          </div>
+        }
+        rightIconAction={() => console.log("bell clicked")}
+      />
+    </div>
+  );
 };
 
 export default SupportPage;
