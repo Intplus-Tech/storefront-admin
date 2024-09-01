4a85efd053ec53b0319c339f5147a496c4f1c620
Amiolemen David <david@piqy.co.uk>
2024-09-01 12:07:19 +0100

    added the dashboard headers to the screens

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,19 +3,20 @@ import LeftSidebar from "@/components/sidebar/leftsidebar";
 
 export default function MainLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <div>
-      <div className=" w-screen  flex flex-col justify-between">
-        <DashboardHeader />
-        <div className="hidden  h-full md:flex flex-col w-60 fixed inset-y-0 z-50 mt-20">
+    <div className="flex flex-col h-screen w-screen">
+      <DashboardHeader />
+      <div className="flex flex-1 pt-20">
+        {/* pt-20 to make space for the header */}
+        {/* Sidebar beneath the header */}
+        <div className="flex-none w-60">
           <LeftSidebar />
         </div>
-        <div className="w-full h-screen md:ml-60 pl-12 pt-[120px] bg-gray-100 ">
-          {children}
-        </div>
+        {/* Content area */}
+        <main className="flex-1 p-6 bg-gray-100 overflow-auto">{children}</main>
       </div>
     </div>
   );
--- app/(dashboard)/overview/page.tsx
+++ app/(dashboard)/overview/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+import DashboardCommonHeader from "@/components/common/dashboard-common-header";
+import { Settings, Settings2 } from "lucide-react";
 import React from "react";
 
 type Props = {};
 
 const OverviewPage = (props: Props) => {
-  return <div className=" h-screen ">Dashboard Overview</div>;
+  return (
+    <div className=" h-screen  w-full ">
+      <DashboardCommonHeader
+        title="Overview"
+        rightIcon={
+          <div
+            onClick={() => console.log("click")}
+            className=" p-3 border space-x-2 cursor-pointer border-[#979797] text-[#979797] border-gray-3  bg-white flex rounded-md items-center justify-center"
+          >
+            <Settings size={16} color="#979797" />
+            <p>Manage</p>
+          </div>
+        }
+        rightIconAction={() => console.log("bell clicked")}
+      />
+    </div>
+  );
 };
 
 export default OverviewPage;
